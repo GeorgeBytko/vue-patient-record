1872de74f3bc27b4d432bead738cbc5627f57175
GeorgeBitcoin <selenich228@mail.ru>
2020-07-25 14:40:31 +0300

    extra code deleted

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const history = require('connect-history-api-fallback');
 const PORT = process.env.PORT || 3000
 const app = express()
 
-const UPDATE_FEEDS_TIMEOUT = 60*60*1000
-
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 console.log(path.join(__dirname, '../dist'))
@@ -15,10 +13,6 @@ app.use( express.static(path.join(__dirname, '../dist')))
 app.use(router)
 app.use(history())
 app.use( express.static(path.join(__dirname, '../dist')))
-/*app.get('/lol', async (req, res) => {
-        res.sendFile(path.join(__dirname, '../dist/index.html'))
-
-})*/
 
 async function start() {
   try {
